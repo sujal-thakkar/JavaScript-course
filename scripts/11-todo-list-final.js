31494f2851af275cb14d0a14b08f22f5746de962
Sujal Thakkar <sujalthakkar153@gmail.com>
2025-02-18 08:35:05 +0530

    add final version of todo-list

--- scripts/11-todo-list-final.js
+++ scripts/11-todo-list-final.js
@@ -15,20 +15,20 @@ function printTodo2() {
     for(let i=0; i<todoList3.length; i++) {
         const todo2 = todoList3[i];
         const html2 = `
-        <p>
-            <span class="todo-text">${todo2.name}</span>
-            <span class="due-date-text">${todo2.dueDate}</span>
-            <button 
-                class="dlt-btn"
-                onclick="
-                    todoList3.splice(${i}, 1);
-                    printTodo2();
-                ">
-                Delete
-            </button>
-        </p>`;
+        <div>${todo2.name}</div>
+        <div>${todo2.dueDate}</div>
+        <button 
+            class="dlt-btn"
+            onclick="
+                todoList3.splice(${i}, 1);
+                printTodo2();
+            ">
+            Delete
+        </button>
+        `;
         todosHTML2 += html2;
     }
     todos2.innerHTML = todosHTML2;
     todoInput3.value = '';
+    todoDate.value = '';
 }
